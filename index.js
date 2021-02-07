0cec8bcef70bd2ee817fa9df03e36aefdb5bea33
root <rasigacalin@gmail.com>
2021-02-07 17:37:57 +0200

    Materia Design Button

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $(function(){
-  
     $('.form-control').each(function(){
       changeState($(this));
     });
@@ -26,3 +25,30 @@ $(function(){
         passwordInput.attr("type",t);
     });
 });
+
+function createRipple(event) {
+  const button = event.currentTarget;
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
+
+const buttons = document.getElementsByTagName("button");
+console.log(buttons.length)
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}
